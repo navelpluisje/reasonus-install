a46f58b16ad4de9a80bcfe9cc33b423eb9a210a0
Erwin Goossen <erwin@navelpluisje.nl>
2022-08-10 22:49:42 +0200

    Added a fix for too many csurf lines

--- src/main/api/utils/addCsiToReaperIni.ts
+++ src/main/api/utils/addCsiToReaperIni.ts
@@ -4,12 +4,42 @@ import path from 'path';
 
 import { settings } from '../../../utils/settings';
 
+const cleanupUnusedCsiLines = (ini: any) => {
+  const regex = /^csurf_([0-9]{1,3})/;
+  let modified = false;
+
+  const maxIndex = parseInt(ini.REAPER.csurf_cnt, 10) - 1 || 0;
+  const reaperIni =  Object.entries(ini.REAPER).reduce((acc, [key, value]) => {
+    const match = key.match(regex);
+    if (parseInt(match?.[1] || '0') > maxIndex) {
+      return acc;
+    }
+    modified = true;
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+
+  return {
+    modified: modified,
+    reaperIni: {
+      ...ini,
+      REAPER: reaperIni,
+    },
+  }
+  ;
+};
+
+
 export const addCsiToReaperIni = () => {
   const reaperPath = settings.get('reaperPath') as string;
 
   try {
-    const reaperIni = ini.decode(fs.readFileSync(path.join(reaperPath, 'reaper.ini'), 'utf8'));
-    const index = parseInt(reaperIni.REAPER.csurf_cnt, 10) || 0;
+    const rawIni = ini.decode(fs.readFileSync(path.join(reaperPath, 'reaper.ini'), 'utf8'));
+    const index = parseInt(rawIni.REAPER.csurf_cnt, 10) || 0;
+
+    const {reaperIni, modified} = cleanupUnusedCsiLines(rawIni) as typeof rawIni;
 
     const hasCSI = new Array(index).fill(undefined).some(((_, i) => {
       if ((reaperIni.REAPER[`csurf_${i}`] as string).indexOf('CSI') !== -1) {
@@ -23,6 +53,9 @@ export const addCsiToReaperIni = () => {
       // eslint-disable-next-line camelcase
       reaperIni.REAPER.csurf_cnt = `${index + 1}`;
     
+      fs.writeFileSync(path.join(reaperPath, 'reaper.ini'), ini.encode(reaperIni));
+    }
+    if (modified) {
       fs.writeFileSync(path.join(reaperPath, 'reaper.ini'), ini.encode(reaperIni));
     }
 
